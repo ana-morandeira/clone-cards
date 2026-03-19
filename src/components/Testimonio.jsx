9a88e731ddb2e01d6586fc9793fc76ba9de16203
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-03-19 19:00:14 +0100

    feat: final project version with all images working

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,32 +1,30 @@
+import React from 'react';
 import '../styles/Testimonio.css';
 
-const imagenes = import.meta.glob('../assets/img/*.png', {
-  eager: true,
-  import: 'default'
-});
-
 function Testimonio(props) {
+  // Esta línea construye la ruta correcta para Vite
+  const rutaImagen = new URL(`../assets/img/Testimonio-${props.imagen}.png`, import.meta.url).href;
+
   return (
     <div className='contenedor-testimonio'>
       <img 
         className='imagen-testimonio'
-        src={imagenes[`../assets/img/${props.imagen}.png`]}
-        alt={`Foto de ${props.nombre}`}
-         />
-
+        src={rutaImagen} 
+        alt={`Foto de ${props.nombre}`} 
+      />
       <div className='contenedor-texto-testimonio'>
         <p className='nombre-testimonio'>
-            <strong>{props.nombre}</strong> en {props.pais}
+          <strong>{props.nombre}</strong> en {props.pais}
         </p>
         <p className='cargo-testimonio'>
-            {props.cargo} en <strong>{props.empresa}</strong>
+          {props.cargo} en <strong>{props.empresa}</strong>
         </p>
         <p className='texto-testimonio'>
-            "{props.testimonio}"
+          "{props.testimonio}"
         </p>
       </div>
     </div>
-    );
+  );
 }
 
 export default Testimonio;
